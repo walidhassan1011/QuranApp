753ad20998a9095a56b0c491d4f1ada71fa154de
walidhassan1011 <walidhassan009@gmail.com>
2023-07-08 18:38:44 +0300

    fixed bugs

--- components/dropdown/DropDown.jsx
+++ components/dropdown/DropDown.jsx
@@ -3,7 +3,7 @@ import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 const DropDown = ({ children, onChange, style }) => {
   return (
-    <select className={...style} onChange={onChange}>
+    <select className={style} onChange={onChange}>
       {children}
     </select>
   );
