bd40d3f3c2796504cd1005a3d549ae2132c643c9
walidhassan1011 <walidhassan009@gmail.com>
2023-08-25 22:41:15 +0300

    add prod env and change api

--- api/api.js
+++ api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_WEB_URL}`,
+  baseURL: `${process.env.NEXT_PUBLIC_WEB_URL_PROD}`,
 });
 
 export default API;
